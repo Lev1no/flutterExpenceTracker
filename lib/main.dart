a1502fe1d9322c2add1dc6a468c2750b02dd3692
Leevi Alanen <leevi.alanen1@gmail.com>
2024-06-17 11:54:26 +0300

    creating first screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/expenses.dart';
+
 void main() {
   runApp(
-    MaterialApp(
-      home: 
+    const MaterialApp(
+      home: Expenses(),
     ),
   );
 }
-
