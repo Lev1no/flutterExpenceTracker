150cf0ff30fcc0edae5dd932e13d40646ebb2667
Leevi Alanen <leevi.alanen1@gmail.com>
2024-10-09 14:09:44 +0300

    error handling for input

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -31,6 +31,12 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty) {}
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -116,10 +122,7 @@ class _NewExpenseState extends State<NewExpense> {
                 child: const Text('Cancel'),
               ),
               ElevatedButton(
-                onPressed: () {
-                  print(_titleController.text);
-                  print(_amountController.text);
-                },
+                onPressed: _submitExpenseData,
                 child: const Text('Save Expense'),
               ),
             ],
